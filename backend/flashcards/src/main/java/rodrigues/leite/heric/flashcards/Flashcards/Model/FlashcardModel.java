cf92d6a1232f94f153638998deb33ff18c572642
Heric Leite Rodrigues <hericleiterodrigues@gmail.com>
2025-10-04 18:53:30 -0300

    Feat: adding flashcards and folders tables

---
+++ backend/flashcards/src/main/java/rodrigues/leite/heric/flashcards/Flashcards/Model/FlashcardModel.java
@@ -0,0 +1,35 @@
+package rodrigues.leite.heric.flashcards.Flashcards.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import rodrigues.leite.heric.flashcards.Folders.Model.FolderModel;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_cards")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FlashcardModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "face_text", nullable = false)
+    private String faceText;
+
+    @Column(name = "back_text", nullable = false)
+    private String backText;
+
+    @Column(name = "optional_hint")
+    private String optionalHint;
+
+    @ManyToMany(mappedBy = "flashcards")
+    private Set<FolderModel> folders = new HashSet<>();
+}
---
+++ backend/flashcards/src/test/java/rodrigues/leite/heric/flashcards/FlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package rodrigues.leite.heric.flashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
