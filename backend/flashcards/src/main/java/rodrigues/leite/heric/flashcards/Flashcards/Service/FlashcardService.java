0135967e39da049a75bdc9eef33fb299f39ef136
Heric Leite Rodrigues <heric.rodrigues@inf.ufrgs.br>
2025-10-04 22:08:50 -0300

    Feat: adding API routes (#8)
    * Feat: adding flashcard save route and service
    
    * Feat: adding get flashcard route
    
    * Feat: adding update flashcard route
    
    * Feat: adding delete flashcard route
    
    * Fea: adding folders save route
    
    * Feat: adding get folder route
    
    * Feat: adding update folder route
    
    * Feat: adding delete folder route

---
+++ backend/flashcards/src/main/java/rodrigues/leite/heric/flashcards/Flashcards/Service/FlashcardService.java
@@ -0,0 +1,42 @@
+package rodrigues.leite.heric.flashcards.Flashcards.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import rodrigues.leite.heric.flashcards.Flashcards.Model.FlashcardModel;
+import rodrigues.leite.heric.flashcards.Flashcards.Repository.FlashcardRepository;
+
+import java.util.Optional;
+
+@Service
+public class FlashcardService {
+
+    @Autowired
+    private FlashcardRepository flashcardRepository;
+
+    public FlashcardModel saveFlashcard(FlashcardModel flashcard) {
+        return this.flashcardRepository.save(flashcard);
+    }
+
+    public Optional<FlashcardModel> getFlashcardById(Long id) {
+        return this.flashcardRepository.findById(id);
+    }
+
+    public Optional<FlashcardModel> updateFlashcard(Long id, FlashcardModel flashcard) {
+        Optional<FlashcardModel> existingFlashcardOptional = this.getFlashcardById(id);
+
+        if (existingFlashcardOptional.isPresent()) {
+            FlashcardModel existingFlashcard = existingFlashcardOptional.get();
+            existingFlashcard.setFaceText(flashcard.getFaceText());
+            existingFlashcard.setBackText(flashcard.getBackText());
+            existingFlashcard.setOptionalHint(flashcard.getOptionalHint());
+
+            return Optional.of(this.saveFlashcard(existingFlashcard));
+        }
+
+        return Optional.empty();
+    }
+
+    public void deleteFlashcard(Long id) {
+        this.flashcardRepository.deleteById(id);
+    }
+}
---
+++ backend/flashcards/src/main/java/rodrigues/leite/heric/flashcards/Folders/Controller/FolderController.java
@@ -0,0 +1,43 @@
+package rodrigues.leite.heric.flashcards.Folders.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import rodrigues.leite.heric.flashcards.Folders.Model.FolderModel;
+import rodrigues.leite.heric.flashcards.Folders.Service.FolderService;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/api/folders")
+public class FolderController {
+
+    @Autowired
+    private FolderService folderService;
+
+    @PostMapping("/save")
+    public ResponseEntity<FolderModel> saveFolder(@RequestBody FolderModel folder) {
+        return ResponseEntity.ok(this.folderService.saveFolder(folder));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<FolderModel>> getFolderById(@PathVariable Long id) {
+        return ResponseEntity.ok(this.folderService.getFolderById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Optional<FolderModel>> updateFolder(@PathVariable Long id, @RequestBody FolderModel folder) {
+        return ResponseEntity.ok(this.folderService.updateFolder(id, folder));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteFolder(@PathVariable Long id) {
+        if (this.folderService.getFolderById(id).isPresent()) {
+            this.folderService.deleteFolder(id);
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+}
