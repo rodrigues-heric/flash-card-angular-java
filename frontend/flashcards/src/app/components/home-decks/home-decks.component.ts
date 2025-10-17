765b81c59e412aef2bfbca9307ab4917f8aaa2fc
Heric Leite Rodrigues <hericleiterodrigues@gmail.com>
2025-10-17 17:01:29 -0300

    Feat: adding deck removal

--- frontend/flashcards/src/app/components/home-decks/home-decks.component.ts
+++ frontend/flashcards/src/app/components/home-decks/home-decks.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
-import { IDeck } from 'src/app/services/decks.service';
+import { DecksService, IDeck } from 'src/app/services/decks.service';
 
 @Component({
   selector: 'app-home-decks',
@@ -10,12 +10,24 @@ import { IDeck } from 'src/app/services/decks.service';
 export class HomeDecksComponent {
   @Input() hasDecks: boolean = false;
   @Input() decks: IDeck[] = [];
+  @Input() isRemoveOption: boolean = false;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private decksService: DecksService) {}
 
   public navigateToUpdateDeck(deck: IDeck): void {
     this.router.navigate(['/update-deck', deck.id], {
       state: { deckData: deck },
+    });
+  }
+
+  public onRemoveDeck(id: number): void {
+    this.decksService.deleteDeck(id).subscribe({
+      next: () => {
+        this.decks = this.decks.filter((deck) => deck.id !== id);
+      },
+      error: (err) => {
+        console.error('Error deleting deck:', err);
+      },
     });
   }
 }
